14d044f898ffaa71740e144fd673004b1465011b
Deval <29129220+1Deval@users.noreply.github.com>
2021-12-12 21:38:13 -0500

    partial solutions

--- src/main/java/advent/utils/Pair.java
+++ src/main/java/advent/utils/Pair.java
@@ -8,7 +8,6 @@ public final class Pair<F, S> {
     private final F firstValue;
     private final S secondValue;
     private static final String TO_STRING = "Pair [{0}, {1}]";
-    private final int hash = Integer.MAX_VALUE;
 
     /**
      * Construct a Pair using the given values
