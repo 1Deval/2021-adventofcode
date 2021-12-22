4299b87977443131195b71000e1982fdb3d35937
Deval <29129220+1Deval@users.noreply.github.com>
2021-12-22 11:13:01 -0500

    partial solutions

--- src/main/java/advent/day17/Main.java
+++ src/main/java/advent/day17/Main.java
@@ -3,24 +3,54 @@ package advent.day17;
 import advent.read.Util;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
     public static void main(final String... args) throws IOException {
-        final List<String> data = Util.getData("day17/sample.txt");
+        final List<String> data = Util.getData("day17/input.txt");
         final Range xRange = getRange("x", data.get(0));
         final Range yRange = getRange("y", data.get(0));
 
 
         final int dip = Math.abs(yRange.min);
 
-        final int yMax = dip * (dip + 1) / 2 - dip;
-        final int xMin = (int) ((-1 + Math.sqrt(1 + 8 * xRange.min))/2);
-        System.out.println("P1:" + yMax);
+        final int yMaxPosition = dip * (dip + 1) / 2 - dip;
+        final int yMax = getInitVelocityForMax(yMaxPosition);
+        final int xMin = getInitVelocityForMax(xRange.min);
+        System.out.println("P1:" + yMaxPosition);
         System.out.println("yMax:" + yMax);
         System.out.println("xMin:" + xMin);
+
+        final List<Probe> probes = new ArrayList<>();
+        for (int x = xMin; x <= xRange.max; x++) {
+            for (int y = yRange.min; y <= yMax; y++) {
+                probes.add(new Probe(x, y, xRange, yRange));
+            }
+        }
+        int inRange = 0;
+        while(!probes.isEmpty()) {
+            final Iterator<Probe> iterator = probes.iterator();
+            while (iterator.hasNext()) {
+                final Probe probe = iterator.next();
+                final Probe.Bounds bounds = probe.step();
+                if (bounds == Probe.Bounds.IN) {
+                    iterator.remove();
+                    inRange++;
+                } else if (bounds == Probe.Bounds.OUT) {
+                    iterator.remove();
+                }
+            }
+        }
+        System.out.println(inRange);
+    }
+
+
+    private static int getInitVelocityForMax(final int yMaxPosition) {
+        return (int) ((-1 + Math.sqrt(1 + 8 * yMaxPosition)) / 2);
     }
 
     private static Range getRange(final String label, final String line) {
--- src/main/java/advent/day17/Probe.java
+++ src/main/java/advent/day17/Probe.java
@@ -5,4 +5,34 @@ public class Probe {
     int y;
     int xVelocity;
     int yVelocity;
+    final Range xRange;
+    final Range yRange;
+
+    public Probe(final int xVelocity, final int yVelocity, final Range xRange, final Range yRange) {
+        this.xVelocity = xVelocity;
+        this.yVelocity = yVelocity;
+        this.xRange = xRange;
+        this.yRange = yRange;
+    }
+
+    enum Bounds {
+        OUT, IN, UNKNOWN
+    }
+
+    Bounds step() {
+        x += xVelocity;
+        y += yVelocity;
+        if (xVelocity != 0) {
+            xVelocity --;
+        }
+        yVelocity--;
+
+        if (x > xRange.max || y < yRange.min) {
+            return Bounds.OUT;
+        }
+        if (x >= xRange.min && y <= yRange.max) {
+            return Bounds.IN;
+        }
+        return Bounds.UNKNOWN;
+    }
 }
